b161606fc5b0fcf92580b424fe1ed53c6ce15dbe
Ayu Nakata <mieze018@users.noreply.github.com>
2023-02-06 04:21:51 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import { Icon } from '@/components/___Atoms/Icon'
 
+const TestComponent = tw.div`text-2xl`
 export default function Home() {
   return (
     <div tw="bg-black text-white flex">
       Home
       <Icon name="ChevronDown" />
+      <TestComponent>Test</TestComponent>
     </div>
   )
 }
