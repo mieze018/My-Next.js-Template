a1de5e13b8f9066d3eb204657c44bb7ed0bef129
Ayu Nakata <mieze018@users.noreply.github.com>
2023-02-06 04:08:47 +0900

    Update GlobalStyles.tsx

--- styles/GlobalStyles.tsx
+++ styles/GlobalStyles.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { createGlobalStyle } from 'styled-components'
-import tw, { theme } from 'twin.macro'
+// import tw, { theme } from 'twin.macro'
 // https://github.com/arlyon/stailwc/issues/23 が解決するまでグローバルスタイルを読み込めないのでcssに書く
 const CustomStyles = createGlobalStyle({
-  body: {
-    WebkitTapHighlightColor: theme`colors.primary`,
-    ...tw`antialiased text-white bg-black`,
-  },
+  // body: {
+  //   WebkitTapHighlightColor: theme`colors.primary`,
+  //   ...tw`antialiased text-white bg-black`,
+  // },
 })
 
 const GlobalStyles = () => (
