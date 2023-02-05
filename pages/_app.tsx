9b5f5aad8bb62d15741e66e3d5de7deaf915b5bb
Ayu Nakata <mieze018@users.noreply.github.com>
2023-02-06 00:21:01 +0900

    プリフライトをCDNから取得、グローバルスタイルを読み込めないのでcssに書く

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,17 @@
 import '@/styles/globals.css'
-import GlobalStyles from '../styles/GlobalStyles'
+// import GlobalStyles from '../styles/GlobalStyles'
 
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GlobalStyles />
+      {/* <GlobalStyles /> */}
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://unpkg.com/tailwindcss@3.1.8/src/css/preflight.css"
+      />
       <Component {...pageProps} />
     </>
   )
--- styles/GlobalStyles.tsx
+++ styles/GlobalStyles.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createGlobalStyle } from 'styled-components'
 import tw, { theme } from 'twin.macro'
-
+// https://github.com/arlyon/stailwc/issues/23 が解決するまでグローバルスタイルを読み込めないのでcssに書く
 const CustomStyles = createGlobalStyle({
   body: {
     WebkitTapHighlightColor: theme`colors.primary`,
